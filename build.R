7ce25f0a1d1fca752ebef4511019f25e75b45d3b
Sebastian <wahani@gmail.com>
2015-06-05 14:59:51 +0200

    init useR! 2015

---
+++ build.R
@@ -0,0 +1,14 @@
+library(knitr)
+args <- commandArgs(TRUE)
+input <- args[1]
+output <- args[2]
+figPath <- paste0("figs/", sub(".Rmd$", "", basename(input)), "/")
+opts_chunk$set(fig.path = figPath)
+opts_chunk$set(fig.show = "hide")
+# opts_chunk$set(fig.cap = "center")
+opts_chunk$set(tidy = FALSE)
+# opts_chunk$set(out.width = "\\textwidth")
+opts_chunk
+opts_chunk$set(fig.width=7)
+opts_chunk$set(fig.height=4)
+knit(input = args[1], output = args[2])
